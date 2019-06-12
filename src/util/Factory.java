a5cf2cd267864c6d0bf1c6b2e0b771a0aff7ef83
manish-kum <mailatmanish@gmail.com>
2019-06-12 12:29:26 +0530

    Update Factory.java
    Replaced custom implementation of Instance with Supplier functional interface

--- src/util/Factory.java
+++ src/util/Factory.java
@@ -2,21 +2,21 @@ package util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class Factory<T> {
-	private Map<String, Instance<T>> factory = new HashMap<>();
+	private Map<String, Supplier<T>> factoryMap = new HashMap<>();
 
 	public synchronized T getEncryptor(String key) {
-
-		Instance<T> test = factory.get(key);
+		Supplier<T> instanceSupplier = factoryMap.get(key);
 		if (test != null)
-			return test.getInstance();
+			return instanceSupplier.get();
 		else
 			return null;
 	}
 
-	public synchronized void put(String key, Instance<T> value) {
-		factory.put(key, value);
+	public synchronized void put(String key, Supplier<T> value) {
+		factoryMap.put(key, value);
 
 	}
 }
